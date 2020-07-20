8ff77239e1d642a5f158ba285e4cd077becde9bf
douglasmatosdev <douglasmatosdev@gmail.com>
2020-07-20 13:17:14 -0300

    Resolved problem 1004

--- 1004/index.js
+++ 1004/index.js
@@ -1,2 +1,8 @@
 let input = require('fs').readFileSync('stdin', 'utf8');
 lines = input.split('\n');
+
+let [A, B] = lines.map(item => parseInt(item));
+
+let PROD = A * B;
+
+console.log('PROD =', PROD);
