6cc5c85f4098a547a0bb540ca35f9a28d9055dd8
douglasmatosdev <douglasmatosdev@gmail.com>
2020-07-20 13:51:57 -0300

    resolved 1005

--- 1005/index.js
+++ 1005/index.js
@@ -1,2 +1,13 @@
 let input = require('fs').readFileSync('stdin', 'utf8');
 lines = input.split('\n');
+
+let [A, B] = lines.map(item => parseFloat(item));
+
+let AWeight = 3.5;
+let BWeight = 7.5;
+
+let weight = AWeight + BWeight;
+
+let MEDIA = ((( (A * AWeight) + (B * BWeight) ) / weight)).toFixed(5);
+
+console.log('MEDIA =', MEDIA);
