c310876d06025f7725854b4ca7ba8e2ecb448263
douglasmatosdev <douglasmatosdev@gmail.com>
2020-07-21 10:52:17 -0300

    resolved 1011

--- 1011/index.js
+++ 1011/index.js
@@ -1,2 +1,9 @@
 let input = require('fs').readFileSync('stdin', 'utf8');
 lines = input.split('\n');
+
+let pi = Math.PI.toFixed(5);
+let R = parseInt(lines);
+
+let VOLUME = ((4 / 3) * pi * Math.pow(R, 3)).toFixed(3);
+
+console.log('VOLUME =', VOLUME);
