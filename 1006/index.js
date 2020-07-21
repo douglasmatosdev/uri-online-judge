19e18cdd011efe2823679534ed8e4cf9f1e7fe25
douglasmatosdev <douglasmatosdev@gmail.com>
2020-07-21 08:19:08 -0300

    Resolved 1006

--- 1006/index.js
+++ 1006/index.js
@@ -1,2 +1,12 @@
 let input = require('fs').readFileSync('stdin', 'utf8');
 lines = input.split('\n');
+
+let [A, B, C] = lines.map(item => parseFloat(item));
+let AWeight = 2;
+let BWeight = 3;
+let CWeight = 5;
+let weight = AWeight + BWeight + CWeight;
+
+let MEDIA = (((A * AWeight) + (B * BWeight) + (C * CWeight)) / weight).toFixed(1);
+
+console.log('MEDIA =', MEDIA);
