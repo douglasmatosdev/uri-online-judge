490611d0d1e6925dcadb354bf09d687ba4dadb94
douglasmatosdev <douglasmatosdev@gmail.com>
2020-07-21 09:53:22 -0300

    resolved 1008

--- 1008/index.js
+++ 1008/index.js
@@ -1,2 +1,8 @@
 let input = require('fs').readFileSync('stdin', 'utf8');
 lines = input.split('\n');
+
+let [NUMBER, HOURS, TIMEVALUE] = lines.map(item => item);
+
+let SALARY = (parseFloat(TIMEVALUE) * HOURS).toFixed(2);
+
+console.log(`NUMBER = ${NUMBER}\nSALARY = U$ ${SALARY}`);
