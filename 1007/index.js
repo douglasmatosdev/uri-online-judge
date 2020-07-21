4b021d0915c96827ba34fca0873235cca72fd98c
douglasmatosdev <douglasmatosdev@gmail.com>
2020-07-21 08:27:08 -0300

    RESOLVED 1007

--- 1007/index.js
+++ 1007/index.js
@@ -1,2 +1,8 @@
 let input = require('fs').readFileSync('stdin', 'utf8');
 lines = input.split('\n');
+
+let [A, B, C, D] = lines.map(item => parseInt(item));
+
+let DIFFERENCE = (A * B) - (C * D);
+
+console.log('DIFERENCA =', DIFFERENCE);
