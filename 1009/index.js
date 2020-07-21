f5d59cf4166029c1636c7e3b2ecce93e1e07df52
douglasmatosdev <douglasmatosdev@gmail.com>
2020-07-21 10:14:04 -0300

    resolved 1009

--- 1009/index.js
+++ 1009/index.js
@@ -1,2 +1,11 @@
 let input = require('fs').readFileSync('stdin', 'utf8');
 lines = input.split('\n');
+
+let [SELLER, SALARY, SALES] = lines.map((item, i) => i !== 0 ? parseFloat(item) : item);
+
+let rate = 15;
+let comission = (rate * SALES) / 100;
+
+let TOTAL = (SALARY + comission).toFixed(2);
+
+console.log('TOTAL = R$', TOTAL);
