21792d82e57feed43a2ae42d396f10064e8cd637
douglasmatosdev <douglasmatosdev@gmail.com>
2020-07-21 10:39:34 -0300

    resolved 1010

--- 1010/index.js
+++ 1010/index.js
@@ -1,2 +1,13 @@
+const { parse } = require('url');
+
 let input = require('fs').readFileSync('stdin', 'utf8');
 lines = input.split('\n');
+
+let [PROD1, PROD2] = lines.map(item => item.split(' ').map((e, i) => i == 2 ? parseFloat(e) : parseInt(e)));
+
+let [CODE1, UNIT1, PRICE1] = PROD1.map((item, i) => item);
+let [CODE2, UNIT2, PRICE2] = PROD2.map((item, i) => item);
+
+let VALUE = ((UNIT1 * PRICE1) + (UNIT2 * PRICE2)).toFixed(2);
+
+console.log('VALOR A PAGAR: R$', VALUE);
