77cee45a06bc6077250612481a369b2b6826a4e9
douglasmatosdev <douglasmatosdev@gmail.com>
2020-07-28 09:25:05 -0300

    Resolved challenge 1013

--- 1013/index.js
+++ 1013/index.js
@@ -1,2 +1,14 @@
 let input = require('fs').readFileSync('stdin', 'utf8');
 lines = input.split('\n');
+
+let [a, b, c] = lines[0].split(' ').map(item => parseInt(item));
+
+let Maior = (a, b, c) => {
+    
+    let dif = (a + b + (Math.abs(a - b))) / 2;
+
+    return (dif + c + (Math.abs(dif - c))) / 2;
+}
+
+
+console.log(`${Maior(a, b, c)} eh o maior`);
