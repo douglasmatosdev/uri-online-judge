b5ea593ed8211c006b157eaa6baba2649ab30db4
douglasmatosdev <douglasmatosdev@gmail.com>
2020-07-28 09:32:26 -0300

    resolved 1014

--- 1014/index.js
+++ 1014/index.js
@@ -1,2 +1,8 @@
 let input = require('fs').readFileSync('stdin', 'utf8');
 lines = input.split('\n');
+
+
+let [x, y] = lines.map((e, i) => i === 0 ? parseInt(e) : parseFloat(e));
+let averageConsuption = (x / y).toFixed(3);
+
+console.log(`${averageConsuption} km/l`);
