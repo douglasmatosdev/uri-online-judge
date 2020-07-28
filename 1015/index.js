ad5fae3b7f49deceaf11d556e93dea36847582b1
douglasmatosdev <douglasmatosdev@gmail.com>
2020-07-28 10:21:11 -0300

    Resolved 1015

--- 1015/index.js
+++ 1015/index.js
@@ -1,2 +1,9 @@
 let input = require('fs').readFileSync('stdin', 'utf8');
 lines = input.split('\n');
+
+let [x1, y1] = lines[0].split(' ').map(e => parseFloat(e));
+let [x2, y2] = lines[1].split(' ').map(e => parseFloat(e));
+
+let distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)).toFixed(4);
+
+console.log(distance);
