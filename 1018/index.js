fb52528facc3e161af3983a2c7905b87cabad8ae
douglasmatosdev <douglasmatosdev@gmail.com>
2021-11-27 14:29:22 -0300

    Solving problem 1018

--- 1018/index.js
+++ 1018/index.js
@@ -1,2 +1,55 @@
 let input = require('fs').readFileSync('stdin', 'utf8');
 lines = input.split('\n');
+
+const POSSIBLE_BANKNOTES = [100, 50, 20, 10, 5, 2, 1];
+let value = parseInt(lines.shift());
+const necessaryBanknotes = POSSIBLE_BANKNOTES.reduce((previousValue, banknote) => {
+    let count = 0;
+    while (value - banknote >= 0) {
+        count = count + 1;
+        value = value - banknote;
+    }
+    return [...previousValue, `${count} nota(s) de R$ ${banknote},00`];
+}, [value]);
+necessaryBanknotes.forEach(value => console.log(value));
+
+/**
+ * FIRST METHOD - RETURN A UNIQ STRING
+ */
+// let necessaryBanknotes = `${value}\n`;
+// POSSIBLE_BANKNOTES.forEach((banknote, idx) => {
+//     let count = 0;
+//     while (value - banknote >= 0) {
+//         count = count + 1;
+//         value = value - banknote;
+//     }
+//     necessaryBanknotes = necessaryBanknotes + `${count} nota(s) de R$ ${banknote},00 \n`;
+// });
+// console.log(necessaryBanknotes);
+
+/**
+ * SECOND METHOD - RETURN A UNIQ STRING
+ */
+// const necessaryBanknotes = POSSIBLE_BANKNOTES.reduce((stringResult, banknote) => {
+//     let count = 0;
+//     while (value - banknote >= 0) {
+//         count = count + 1;
+//         value = value - banknote;
+//     }
+//     return stringResult = stringResult + `${count} nota(s) de R$ ${banknote},00 \n`;
+// }, `${value}\n`);
+// console.log(necessaryBanknotes);
+
+/**
+ * THIRD METHOD
+ */
+// const necessaryBanknotes = [`${value}`];
+// for (const idx in POSSIBLE_BANKNOTES) {
+//     let count = 0;
+//     while (value - POSSIBLE_BANKNOTES[idx] >= 0) {
+//         count = count + 1;
+//         value = value - POSSIBLE_BANKNOTES[idx];
+//     }
+//     necessaryBanknotes.push(`${count} nota(s) de R$ ${POSSIBLE_BANKNOTES[idx]},00`);
+// }
+// necessaryBanknotes.forEach(e => console.log(e));
