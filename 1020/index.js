4fea4a2e54a25442e0864eb147b31d6b5f693919
douglasmatosdev <douglasmatosdev@gmail.com>
2021-11-27 16:23:00 -0300

    Solving problem 1020

--- 1020/index.js
+++ 1020/index.js
@@ -1,2 +1,18 @@
 let input = require('fs').readFileSync('stdin', 'utf8');
 lines = input.split('\n');
+
+const YEAR_MONTH_DAY = [365, 30, 1]; // In days
+const STRING_YEAR_MONTH_DAY = ['ano(s)', 'mes(es)', 'dia(s)'];
+
+let age = parseInt(lines.shift());
+
+const ageInDays = YEAR_MONTH_DAY.reduce((previousValue, currentValue, idx) => {
+    let count = 0;
+    while (age - currentValue >= 0) {
+        count = count + 1;
+        age = age - currentValue;
+    }
+    return [...previousValue, `${count} ${STRING_YEAR_MONTH_DAY[idx]}`];
+}, []);
+
+ageInDays.forEach(value => console.log(value));
