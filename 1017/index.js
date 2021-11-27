e794d73517f317a23f6cacd54d50a6bca2bded93
douglasmatosdev <douglasmatosdev@gmail.com>
2021-11-27 11:58:33 -0300

    Solving 1017

--- 1017/index.js
+++ 1017/index.js
@@ -1,2 +1,12 @@
 let input = require('fs').readFileSync('stdin', 'utf8');
 lines = input.split('\n');
+
+const AVERAGE_CONSUMPTION = 12 // 12Km/L
+
+const [hours, speed] = lines.map(item => parseInt(item));
+
+const trevelledDistance = hours * speed
+
+const necessaryFuel = trevelledDistance / AVERAGE_CONSUMPTION
+
+console.log(necessaryFuel.toFixed(3))
