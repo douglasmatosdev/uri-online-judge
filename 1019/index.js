1b5bed9a50fd3b13053996c7ded56eef435a3e4d
douglasmatosdev <douglasmatosdev@gmail.com>
2021-11-27 15:56:27 -0300

    Solving problem 1019

--- 1019/index.js
+++ 1019/index.js
@@ -1,2 +1,15 @@
 let input = require('fs').readFileSync('stdin', 'utf8');
 lines = input.split('\n');
+
+const ONE_HOUR = 3600; // In seconds
+const ONE_MINUTE = 60; // In seconds
+
+let totalSeconds = parseInt(lines.shift());
+
+const hour = Math.floor(totalSeconds / ONE_HOUR);
+
+const minute = Math.floor((totalSeconds % ONE_HOUR) / ONE_MINUTE);
+
+const second = totalSeconds % ONE_MINUTE;
+
+console.log(`${hour}:${minute}:${second}`);
