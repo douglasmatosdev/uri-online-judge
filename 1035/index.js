48f654a539fefaa9792575274d4301b351ec2209
douglasmatosdev <douglasmatosdev@gmail.com>
2021-11-30 08:33:54 -0300

    Solving problem 1035

--- 1035/index.js
+++ 1035/index.js
@@ -1,2 +1,13 @@
 let input = require('fs').readFileSync('stdin', 'utf8');
 lines = input.split('\n');
+
+const [A, B, C, D] = lines.shift().split(' ').map(e => parseInt(e));
+
+let CD = C + D;
+let AB = A + B;
+
+if (B > C && D > A && CD > AB && C >= 0 && D >= 0 && A % 2 === 0) {
+    console.log("Valores aceitos");
+} else {
+    console.log("Valores nao aceitos");
+}
