de6fa0665ffd5ad24810676a482fd6c5f14c77d1
douglasmatosdev <douglasmatosdev@gmail.com>
2021-12-15 00:59:04 -0300

    Solving problem 1021

--- beginner/javascript/1021/index.js
+++ beginner/javascript/1021/index.js
@@ -1,20 +1,21 @@
 let input = require('fs').readFileSync('stdin', 'utf8');
 lines = input.split('\n');
 
-const NOTES = [100.00, 50.00, 20.00, 10.00, 5.00, 2.00];
-const COINS = [1, 0.50, 0.25, 0.10, 0.05, 0.01];
-
 let value = parseFloat(lines.shift().replace(',', '.'));
+const MONEY = [100.0, 50.0, 20.0, 10.0, 5.0, 2.0, 1.0, 0.5, 0.25, 0.10, 0.05, 0.01];
 
-console.log('NOTAS:');
-for (let i = 0; i < NOTES.length; i++) {
-    console.log(`${Math.floor(value / NOTES[i])} nota(s) de R$ ${NOTES[i].toFixed(2)}`);
-    value = value % NOTES[i];
-}
-console.log('MOEDAS:');
-let coin = 0;
-for (let i = 0; i < COINS.length; i++) {
-    coin = Math.floor(value / COINS[i]);
-    console.log(`${coin} moeda(s) de R$ ${COINS[i].toFixed(2)}`);
-    value = value % COINS[i];
-}
+console.log("NOTAS:");
+MONEY.forEach((money, idx) => {
+    let count = 0;
+    while (value >= money) {
+        value = (value - money).toFixed(2);
+        count++;
+    }
+
+    if (money === 1.0) console.log("MOEDAS:");
+
+    if (money >= 2.0)
+        console.log(`${count} nota(s) de R$ ${money.toFixed(2)}`);
+    else
+        console.log(`${count} moeda(s) de R$ ${money.toFixed(2)}`);
+});
