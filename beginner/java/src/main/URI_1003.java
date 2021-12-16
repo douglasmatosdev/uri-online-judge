953d745dfb65f43ece7c868225bb2813048fe89f
douglasmatosdev <douglasmatosdev@gmail.com>
2021-12-16 09:53:39 -0300

    [Java] Solving problem 1003 and 1004

---
+++ beginner/java/src/main/URI_1003.java
@@ -0,0 +1,20 @@
+package main;
+
+import java.util.Scanner;
+
+public class URI_1003 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int SOMA = A + B;
+		
+		System.out.printf("SOMA = %d%n", SOMA);
+		
+		sc.close();
+
+	}
+
+}
---
+++ beginner/java/src/main/URI_1004.java
@@ -0,0 +1,20 @@
+package main;
+
+import java.util.Scanner;
+
+public class URI_1004 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		int PROD = A * B;
+		
+		System.out.printf("PROD = %d%n", PROD);
+		
+		sc.close();
+
+	}
+
+}
